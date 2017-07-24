d005a2d4d854fae2ba1791aafee780ef75c10d7e
EthanShin <avoirom@gmail.com>
2017-07-24 20:46:01 +0900

    modify Finding the percentage.py

--- 2.Basic Data Types/6.Finding the percentage.py
+++ 2.Basic Data Types/6.Finding the percentage.py
@@ -6,3 +6,8 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
+    
+    result = 0
+    for i in student_marks[query_name]:
+        result += i
+    print("%.2f" %(result / 3))
