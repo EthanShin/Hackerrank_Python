a7eda53da104ce619dc2ffd2a4ed1cf92264e90a
EthanShin <avoirom@gmail.com>
2017-07-28 19:44:37 +0900

    add String Formatting.py

---
+++ 3.Strings/10.String Formatting.py
@@ -0,0 +1,8 @@
+def print_formatted(number):
+    # your code goes here
+    for i in range(1, number + 1):
+        print("{0:>{1}d} {0:>{1}o} {0:>{1}X} {0:>{1}b}".format(i, len(str(bin(number)[2:]))))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
